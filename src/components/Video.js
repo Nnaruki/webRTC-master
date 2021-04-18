8e21b4b7098d16088edd1c8b7dc7e6acf026c3ec
Naruki <N1220naruki@icloud.com>
2021-04-18 14:38:20 +0900

    cleanupUIs

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Card ,
         CardActionArea ,
         CardActions ,
@@ -11,13 +10,12 @@ import AudioAnalyser from './AudioAnalyser';
 import useDimensions from './books/useDimentions';
 import VolumeButton from './VolumeButton';
 
-const useStyles = makeStyles({});
-
 const Video = ({ isLocal, name, videoRef, rtcClient }) => {
   const [muted,setMuted] = useState(rtcClient.initialAudioMuted);
   const refCard = useRef(null);
   const dimensionsCard = useDimensions(refCard);
-  const classes = useStyles();
+  const refVolumeButton = useRef(null);
+  const dimensionsVolumeButton = useDimensions(refVolumeButton);
 
   return (
     <Card ref={refCard}>
@@ -35,9 +33,18 @@ const Video = ({ isLocal, name, videoRef, rtcClient }) => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <VolumeButton isLocal={isLocal} muted={muted} rtcClient={rtcClient} setMuted={setMuted} />
+        <VolumeButton 
+          isLocal={isLocal}
+          muted={muted}
+          refVolumeButton={refVolumeButton}
+          rtcClient={rtcClient}
+          setMuted={setMuted} 
+        />
         {!muted && videoRef.current && videoRef.current.srcObject && (
-          <AudioAnalyser audio={videoRef.current.srcObject}/>
+          <AudioAnalyser 
+            audio={videoRef.current.srcObject}
+            width={dimensionsCard.width - dimensionsVolumeButton.width - 40}
+          />
         )}
       </CardActions>
     </Card>
--- src/components/VolumeButton.js
+++ src/components/VolumeButton.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import VolumeOffIcon from '@material-ui/icons/VolumeOff';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
-const VolumeButton = ({ isLocal,muted,rtcClient, setMuted }) => {
+const useState = makeStyles({
+    icon: {
+        height: 38,
+        width: 38,
+    },
+});
+
+const VolumeButton = ({ 
+    isLocal,
+    muted,
+    rtcClient,
+    setMuted,
+    refVolumeButton, 
+}) => {
     const Icon = muted ? VolumeOffIcon : VolumeUpIcon;
 
     return (
@@ -14,8 +28,9 @@ const VolumeButton = ({ isLocal,muted,rtcClient, setMuted }) => {
           setMuted((previousState) => !previousState);
           if (isLocal) rtcClient.toggleAudio();
         }}
+        ref={refVolumeButton}
       >
-        <Icon />
+        <Icon className={classes.icon}/>
       </IconButton>
     );
   };
